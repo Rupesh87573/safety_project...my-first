8b2f564a160ffff6688795c1a2f308b583810cef
Rupesh Kumar Tiwari <158506540+Rupesh87573@users.noreply.github.com>
2026-04-10 00:57:20 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+import smtplib
+from email.message import EmailMessage
+import base64
+
+app = Flask(__name__)
+
+EMAIL_ADDRESS = "kumarrupesh829119@gmail.com"
+EMAIL_PASSWORD = "xszz mrxz jpzh nxjy"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    image = request.form['image']
+    location = request.form['location']
+
+    img_data = base64.b64decode(image.split(',')[1])
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Safety Alert'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = EMAIL_ADDRESS
+    msg.set_content(f"Location: {location}")
+
+    msg.add_attachment(img_data, maintype='image', subtype='png', filename='photo.png')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+
+    return "Sent!"
+    
+app.run(debug=True)
